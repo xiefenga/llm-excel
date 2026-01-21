c85a9eec69fd78c12dc822c176b30851ba767dcd
谢峰 <1294699027@qq.com>
2026-01-21 19:50:59 +0800

    feat: 更新图标

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -11,16 +11,7 @@ import type { Route } from "./+types/root";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+  { rel: "icon", type: "image/svg+xml", href: "/logo.svg" },
 ];
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
